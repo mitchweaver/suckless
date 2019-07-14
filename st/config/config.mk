61be5b4681c77c982c37577562352a86b0dab35f
Mitch Weaver <20451170+MitchWeaver@users.noreply.github.com>
2019-07-14 19:10:31 +0000

    convert st to new format

--- st/config/config.mk
+++ st/config/config.mk
@@ -10,20 +10,13 @@ INCS = -I$(X11INC) \
        `pkg-config --cflags fontconfig` \
        `pkg-config --cflags freetype2`
 
-# LINUX 
 LIBS = -L$(X11LIB) -lm -lrt -lX11 -lutil -lXft \
 		   `pkg-config --libs fontconfig` \
 	       	   `pkg-config --libs freetype2`
-
-# OpenBSD
-#LIBS = -L$(X11LIB) -lm -lX11 -lutil -lXft \
-#       `pkg-config --libs fontconfig` \
-#       `pkg-config --libs freetype2`
 
 CPPFLAGS = -DVERSION=\"$(VERSION)\" -D_XOPEN_SOURCE=600
 STCFLAGS = $(INCS) $(CPPFLAGS) $(CFLAGS)
 STLDFLAGS = $(LIBS) $(LDFLAGS)
 
-CC = tcc -w
 #CC = gcc -w -O3 -pipe -std=c99 -fstack-protector-all -fpie
-# CC = musl-gcc -w -O3 -pipe -std=c99 -fstack-protector-all -fpie
+CC = tcc -w
