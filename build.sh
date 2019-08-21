65f18a718116ba3a4acea73e48df10187970a2e0
Mitch Weaver <mitch@bonsai-linux.org>
2019-08-21 10:05:21 -0500

    update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,17 @@
-#!/bin/sh -e
+#!/bin/sh
 
-download() {
+export CC=gcc
+export CFLAGS='-O3 -pipe -s -w -fstack-protector-strong \
+-fstack-clash-protection -fexceptions -Wl,-z,defs -Wl,-z,now \
+-Wl,-z,relro -Wl,-z,noexecstack'
+export LDFLAGS=-s
+export PREFIX="${HOME}"/usr/local
+
+[ "$1" ] || set -- dmenu sent st surf tabbed
+
+START_PWD="$PWD"
+for name in $@ ; do
+    cd $name
     if [ ! -d $name ] ; then
         git clone https://git.suckless.org/$name
     else
@@ -10,48 +21,13 @@ download() {
         git reset --hard
         cd - > /dev/null
     fi
-}
-
-patchall() {
     ls patches | while read -r patch ; do
         patch -p0 --ignore-whitespace < patches/$patch
     done
-}
-
-build() {
-    cd $name
-    make
-    make install
-    cd - > /dev/null
-}
-
-clean() {
-    cd $name
-    for junk in FAQ.md TODO.md README '*.orig' '*.rej' ; do
-        rm -f $junk 2> /dev/null ||:
-    done
-    make clean
-    cd - > /dev/null
-}
-
-main() {
-    download
-    patchall
     cp -f config/config.h $name/config.h
     cp -f config/config.mk $name/config.mk
-    build
-    clean > /dev/null
-}
-
-[ "$1" = all ] && set -- dmenu sent st surf tabbed
-
-# fetch/build all in parallel
-for arg in "$@" ; do
-    {
-        startdir="$PWD"
-        cd "$arg"
-        name=$arg main
-        cd "$startdir"
-    } &
+    cd $name
+    make
+    make PREFIX="$PREFIX" install
+    cd "$START_PWD"
 done
-wait
