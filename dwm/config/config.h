74735639f785ec867c0a186235263bb5760664a8
Mitch Weaver <mitch@bonsai-linux.org>
2020-02-24 21:55:47 -0600

    dwm: ROUNDED CORNERS - back by popular demand

--- dwm/config/config.h
+++ dwm/config/config.h
@@ -1,10 +1,13 @@
 /* -*--*-*-*-*-*-*-*-*- GAPS -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-* */
 static unsigned int gappx = 12;
-static unsigned int borderpx  = 2;
+static unsigned int borderpx  = 1;
 /* -*-*-*-*-*-*-*-*-*- NO GAPS *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*- */
 /* static unsigned int gappx = 0; */
 /* static const unsigned int borderpx  = 1; */
 /* -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-* */
+
+// for use with the rounded corners patch:
+static const int CORNER_RADIUS = 10;
 
 static const Rule rules[] = {
     /* class             instance    title    tags mask  iscentered   isfloating   monitor */
