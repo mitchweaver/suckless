93aea69f55b207d3085ac150f6f854bd4676cfac
Mitch Weaver <mitch@wvr.sh>
2020-07-25 22:41:36 -0500

    dwm: bump to july 8th

--- dmenu/cfg/config.h
+++ dmenu/cfg/config.h
@@ -1,8 +1,18 @@
-/* static const char *fonts[] = { "Hack:pixelsize=20:antialias=true:autohint=true" }; */
-static const char *fonts[] = { "SF Mono:pixelsize=22:antialias=true:autohint=true" };
-/* static const char *fonts[] = { "ShureTechMono Nerd Font:pixelsize=22:antialias=true:autohint=true" }; */
-/* static const char *fonts[] = { "cozette:size=10:antialias=false:autohint=false" }; */
-/* static const char *fonts[] = { "Terminus:pixelsize=18:antialias=false:autohint=false" }; */
+static const char *fonts[] = {
+    "SF Mono:pixelsize=22:antialias=true:autohint=true",
+    "Noto Color Emoji:pixelsize=22:antialias=true:autohint=true",
+    "Noto Sans:pixelsize=22:antialias=true:autohint=true",
+    "Unifont:pixelsize=22",
+    "ShureTechMono Nerd Font:pixelsize=22:antialias=true:autohint=true"
+};
+    /* "cozette:size=10:antialias=false:autohint=false", */
+    /* "Terminus:pixelsize=18:antialias=false:autohint=false" */
+    /* "Hack:pixelsize=20:antialias=true:autohint=true", */
+    /* "Noto Sans CJK JP:pixelsize=22:antialias=true:autohint=true", */
+    /* "Noto Sans CJK HK:pixelsize=22:antialias=true:autohint=true", */
+    /* "Noto Sans CJK KR:pixelsize=22:antialias=true:autohint=true", */
+    /* "Noto Sans CJK SC:pixelsize=22:antialias=true:autohint=true", */
+    /* "Noto Sans CJK TC:pixelsize=22:antialias=true:autohint=true", */
 
 static const unsigned int border_width = 2;
 static int fuzzy = 1;
