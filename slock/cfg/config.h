277ed55b5a2c31b228420f02689a365b2b32c040
Mitch Weaver <mitch@wvr.sh>
2021-05-04 16:32:18 -0500

    add slock

---
+++ slock/cfg/config.h
@@ -0,0 +1,39 @@
+/* time in seconds to cancel lock with mouse movement */
+static const int timetocancel = 3;
+
+/* allow control key to trigger fail on clear */
+static const int controlkeyclear = 0;
+
+/* user and group to drop privileges to */
+static const char *user  = "nobody";
+static const char *group = "nogroup";
+
+static const char *colorname[NUMCOLS] = {
+	[BACKGROUND] =   "black",     /* after initialization */
+	[INIT] =   "#2d2d2d",     /* after initialization */
+	[INPUT] =  "#005577",   /* during input */
+	[FAILED] = "#CC3333",   /* wrong password */
+};
+
+/* treat a cleared input like a wrong password (color) */
+static const int failonclear = 0;
+
+
+/* insert grid pattern with scale 1:1, the size can be changed with logosize */
+static const int logosize = 75;
+static const int logow = 12;	/* grid width and height for right center alignment*/
+static const int logoh = 6;
+
+static XRectangle rectangles[9] = {
+	/* x	y	w	h */
+	{ 0,	3,	1,	3 },
+	{ 1,	3,	2,	1 },
+	{ 0,	5,	8,	1 },
+	{ 3,	0,	1,	5 },
+	{ 5,	3,	1,	2 },
+	{ 7,	3,	1,	2 },
+	{ 8,	3,	4,	1 },
+	{ 9,	4,	1,	2 },
+	{ 11,	4,	1,	2 },
+
+};
---
+++ slock/cfg/config.mk
@@ -0,0 +1,41 @@
+VERSION = 1.4
+
+# paths
+PREFIX = /usr/local
+MANPREFIX = ${PREFIX}/share/man
+
+X11INC = /usr/X11R6/include
+X11LIB = /usr/X11R6/lib
+
+XINERAMALIBS  = -lXinerama
+XINERAMAFLAGS = -DXINERAMA
+FREETYPELIBS = -lXft
+
+# OpenBSD
+FREETYPEINC = /usr/X11R6/include/freetype2
+# Linux
+# FREETYPEINC = /usr/include/freetype2
+
+INCS = -I. -I/usr/include -I${X11INC} -I${FREETYPEINC}
+
+CFLAGS = -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
+LDFLAGS = -s ${LIBS}
+
+# OpenBSD
+LIBS = -L/usr/lib -lc -L${X11LIB} -lX11 -lXext -lXrandr ${XINERAMALIBS} ${FREETYPELIBS}
+# Linux
+# LIBS = -L/usr/lib -lc -lcrypt -L${X11LIB} -lX11 -lXext -lXrandr ${XINERAMALIBS} ${FREETYPELIBS}
+
+# Linux
+# CPPFLAGS = -DVERSION=\"${VERSION}\" -D_DEFAULT_SOURCE -DHAVE_SHADOW_H ${XINERAMAFLAGS}
+# OpenBSD / FreeBSD
+CPPFLAGS = -DVERSION=\"${VERSION}\" -D_BSD_SOURCE ${XINERAMAFLAGS}
+# NetBSD
+# CPPFLAGS = -DVERSION=\"${VERSION}\" -D_BSD_SOURCE -D_NETBSD_SOURCE ${XINERAMAFLAGS}
+
+# OpenBSD
+COMPATSRC =
+# Linux / FreeBSD / NetBSD
+# COMPATSRC = explicit_bzero.c
+
+CC = cc
